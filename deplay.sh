10a92c9fdf6fc31a519aa8024014c94a864d1a97
Happa8 <happa.eight@gmail.com>
2022-03-04 02:04:20 +0900

    デプロイ用シェルスクリプト

---
+++ deplay.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# エラー時は停止
+set -e
+
+# ビルド
+npm run build
+
+# ビルド出力ディレクトリに移動
+cd dist
+
+# カスタムドメインにデプロイする場合
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# https://<USERNAME>.github.io にデプロイする場合
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# https://<USERNAME>.github.io/<REPO> にデプロイする場合
+git push -f git@github.com:Happa8/KOAN-grade-analyzer.git main:gh-pages
+
+cd -
